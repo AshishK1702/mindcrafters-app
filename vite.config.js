10252a99d55e758a5fe6e04f6fc5f59b4fb984a1
Ash8845 <ashishkhatavkar111@gmail.com>
2025-11-19 11:21:28 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // [https://vitejs.dev/config/](https://vitejs.dev/config/)
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/AshishK1702/mindcrafters-app.git', // <--- ADD THIS LINE
+  base: '/mindcrafters-app/', // <--- ADD THIS LINE
 })
